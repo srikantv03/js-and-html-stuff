a500669db894bd84970de4479350e9aa3b4d7575
treecant <sv092003@gmail.com>
2019-11-03 12:32:53 -0500

    Theme picker
    Part of larger WebApp project

---
+++ Webapp/functions.js
@@ -0,0 +1,14 @@
+function buttonOne(){
+	document.body.style.backgroundColor = "#233039";
+	document.body.style.color = "#7baecc";
+}
+
+function buttonTwo() {
+	document.body.style.backgroundColor = "#d66468";
+	document.body.style.color = "#fcf2b8";
+}
+
+function buttonThree () {
+	document.body.style.backgroundColor = "#47b2a5";
+	document.body.style.color = "#e7a26d"
+}
